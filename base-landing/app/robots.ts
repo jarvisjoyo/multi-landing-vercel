38aa2c3e5ee7ad067d7148d86e6461d2388d8f41
jarvisjoyo <jarvis.joyo@gmail.com>
2025-05-13 02:47:38 +0700

    Update robots.ts
    ok

--- base-landing/app/robots.ts
+++ base-landing/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-    },
+    }, 
     sitemap: `${GlobalConst.domain}/sitemap.xml`,
   }
 }
